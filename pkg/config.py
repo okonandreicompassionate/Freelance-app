d965571c19357f9bb7a9a3f661e861044a04ce55
Andrei Okon <okoncompassionate@gmail.com>
2025-05-19 08:25:21 +0100

    Update config to use DATABASE_URL environment variable

--- pkg/config.py
+++ pkg/config.py
@@ -1,10 +1,14 @@
-
+import os
 
 class BaseConfig(object):
     SECRET_KEY = "59InUNHh4Yqh2yzW_ieqv5CBOJnmW0Z73E2hZhPT8Ss"
     ADMIN_EMAIL = "blogger@blogger.com"
     ADMIN_NAME = "james henry"
-    SQLALCHEMY_DATABASE_URI = 'postgresql://freelance_9we4_user:Geh9UJ8TXAqKAoDZS05ZWKKCpayWUu2V@dpg-d0jraqje5dus73b7v3k0-a.oregon-postgres.render.com/freelance_9we4'
+    # Use environment variable DATABASE_URL if it exists, else fallback to hardcoded URI
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "ppostgresql://neondb_owner:npg_WlT2q6YEZCeH@ep-dawn-snowflake-abnqf7zr-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CONTACT_PHONE = "09088645334"
     API_KEY = "sample key"
